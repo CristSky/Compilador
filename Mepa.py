5b33f6c811c82f1e6923edc4b913398766ba5785
claudiocosta <claudio_costaa@hotmail.com>
2015-11-26 09:23:37 -0200

    v1

--- Mepa.py
+++ Mepa.py
@@ -16,10 +16,19 @@ class Lex():
 
     def tokenize(self, code):
         list = []
-        keywords = {'PROGRAM', 'VAR', 'INTEGER', 'WHILE', 'BEGIN', 'END', 'IF', 'THEN', 'ELSE', 'READ'}
+        keywords = {'PROGRAM', 'VAR', 'INTEGER', 'WHILE', 'BEGIN', 'END', 'IF', 'THEN', 'ELSE', 'READ', 'WRITE'}
         token_specification = [
             ('COMMENT', r'(?://[A-Za-z0-9_]+\n)|(#.*?\n)'),  # comentario com // ou #
             ('NUMBER', r'\d+(\.\d*)?'),  # Integer or decimal number
+            ('ABPAR', r'\('),  # Abre Parenteses
+            ('FEPAR', r'\)'),  # fecha Parenteses
+            ('MEIG', r'<='),  # menor igual
+            ('MAIG', r'>='),  # maior igual
+            ('ME', r'<'),  # menor
+            ('MA', r'>'),  # maior
+            ('DI', r'!='),  # diferente
+            ('IG', r'=='),  # igual
+            ('VIR', r'\,'),  # virgula
             ('ASSIGN', r':='),  # Assignment operator
             ('ENDLINE', r';'),  # Statement terminator
             ('ID', r'[A-Za-z0-9_]+'),  # Identifiers
@@ -28,7 +37,7 @@ class Lex():
             ('SKIP', r'[ \t\r\n]+'),  # Skip over spaces and tabs
             ('MISMATCH', r'./'),  # Any other character
             ('STRING', r'(".*?")'),
-            ('SPECIAL', r'[<>(){}.,\[\]]='),
+            ('SPECIAL', r'[<>{}.\[\]]\='),
         ]
         tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
         line_num = 1
@@ -60,34 +69,114 @@ class Translator():
         self.tok = None
         self.text = ""
 
-    def translate(self, tokens, int):
-        i = int
+    def translate(self, tokens, start, end):
+        i = start
+        e = end
+
         self.tokens = tokens
         size = len(tokens)
-        while i < size:
+        while (i < size):
+            if  (i == e):
+                break
             if self.tokens[i].typ == "PROGRAM":
                 self.INPP()
+                i = self.counter(tokens, i)
             elif self.tokens[i].typ == 'VAR':
                 i = self.AMEM(i + 1)
             elif self.tokens[i].typ == 'READ':
-                self.LEIT(i)
                 i += 2
-            elif (self.tokens[i].typ == 'ID'):
+                while(tokens[i].typ != 'FEPAR'):
+                    self.LEIT(i)
+                    i += 1
+                    if (tokens[i].typ == 'VIR'):
+                        i += 1
+
+                i += 1
+            elif self.tokens[i].typ == 'WRITE':
+                i += 2
+                aux = i
+                con = 0
+
+                while self.tokens[aux].value != ';':
+                    if self.tokens[aux].value == ',':
+                        con += 1
+                    aux += 1
+                z = 0
+
+                while z <= con:
+                    if (tokens[i].typ == 'ID') and ((tokens[i+1].typ == 'VIR')or (tokens[i+1].typ == 'FEPAR')):
+                        self.CRVL(i)
+                        i += 2
+                    elif (tokens[i].typ == 'NUMBER') and ((tokens[i+1].typ == 'VIR') or (tokens[i+1].typ == 'FEPAR')):
+                        self.CRCTI(i)
+                        i += 2
+                    elif (tokens[i+1].typ == 'OP'):
+                        self.NotPolonesa(tokens, i)
+                        while (tokens[i].typ != 'VIR') and (tokens[i].typ != 'FEPAR'):
+                            i += 1
+                        i += 1
+                    self.IMPR()
+
+                    z += 1
+
+            elif self.tokens[i].typ == 'ID':
                 if(self.tokens[i+1].typ == 'ASSIGN') and (self.tokens[i+2].typ == 'NUMBER') and (self.tokens[i+3].typ == 'ENDLINE'):
-                    self.CRCTI(i)
+                    self.CRCTI(i+2)
+                    self.ARMZ(i)
                     i += 3
                 elif(self.tokens[i+1].typ == 'ASSIGN') and (self.tokens[i+2].typ == 'ID') and (self.tokens[i+3].typ == 'ENDLINE'):
                     self.CRVL(i+2)
                     self.ARMZ(i)
-                elif(self.tokens[i+1].typ == 'ASSIGN') and (self.tokens[i+2].typ == 'NUMBER') and (self.tokens[i+3].typ == 'ENDLINE'):
-                    self.CRCTI(i+2)
-                    self.ARMZ(i)
-
+                    i += 3
                 elif(self.tokens[i+1].typ == 'ASSIGN'):
                     self.NotPolonesa(tokens, i+2)
                     self.ARMZ(i)
+                    i = self.counter(tokens, i)
+                elif(self.tokens[i+1].typ == 'OP'):
+                    self.NotPolonesa(tokens, i)
+                    i = self.counter(tokens, i)
+                else:
+                    self.CRVL(i)
 
-            #todo
+            #todo if
+            elif self.tokens[i].typ == 'IF':
+                int1 = i
+                i += 1
+                while self.tokens[i].typ != 'THEN':
+                    if (self.tokens[i].typ == 'ID') and (self.tokens[i+2].typ == 'ID') :
+                        self.CRVL(i)
+                        self.CRVL(i+2)
+                        i += 1
+                    elif (self.tokens[i].typ == 'ID') and (self.tokens[i+2].typ == 'NUMBER'):
+                        self.CRVL(i)
+                        self.CRCTI(i+2)
+                        i += 1
+                    elif (self.tokens[i].typ == 'NUMBER') and (self.tokens[i+2].typ == 'ID'):
+                        self.CRCTI(i)
+                        self.CRVL(i+2)
+                        i += 1
+                    elif (self.tokens[i].typ == 'NUMBER') and (self.tokens[i+2].typ == 'NUMBER'):
+                        self.CRCTI(i)
+                        self.CRCTI(i+2)
+                        i += 1
+                    elif (self.tokens[i].typ == 'MEIG') or (self.tokens[i].typ == 'MAIG') or (self.tokens[i].typ == 'IG') or (self.tokens[i].typ == 'DI') or (self.tokens[i].typ == 'ME') or (self.tokens[i].typ == 'MA'):
+                        self.CompOp(tokens[i].value)
+                        i += 2
+                        self.DSVF(int1)
+                #pega o index do else
+                aux = i
+                while tokens[aux].typ != 'ELSE':
+                    aux += 1
+                self.translate(tokens, i, aux)
+                i = aux
+                self.DSVS(aux)
+                self.NADA(int1)
+                tmp = i
+                while tokens[tmp].typ != 'END':
+                    tmp += 1
+                self.translate(tokens, i, tmp)
+                i = tmp
+                self.NADA(aux)
 
 
 
@@ -98,7 +187,7 @@ class Translator():
                 self.CRVL(i)
                 i += 1
                 self.CRVL(i+1)
-                if self.tokens[i].value == '<=':
+                if (self.tokens[i].typ == 'MEIG') or (self.tokens[i].typ == 'MAIG') or (self.tokens[i].typ == 'IG') or (self.tokens[i].typ == 'DI') or (self.tokens[i].typ == 'ME') or (self.tokens[i].typ == 'MA'):
                     self.CompOp(tokens[i].value)
                 i += 4
                 tmp = i
@@ -114,21 +203,18 @@ class Translator():
 
                 aux = i
                 self.DSVF(i)
-                self.translate(tokens, i)
+                self.translate(tokens, i, tmp)
                 self.DSVS(back)
                 self.NADA(aux)
                 i = tmp
 
-
-            pass
-
-
             i += 1
 
-        ######################
-        for line in self.code:
-            self.text += line
-        return self.text
+        if i == len(tokens):
+            for line in self.code:
+                self.text += line
+            self.text += 'PARA'
+            return self.text
 
     def INPP(self):
         self.code.append("INPP\n")
@@ -145,12 +231,14 @@ class Translator():
         return i
 
     def LEIT(self, index):
-        self.code.append("LEIT\n")
-        self.ARMZ(index+1)
+        self.code.append("LEIT \n")
+        self.ARMZ(index)
+
+    def IMPR(self):
+        self.code.append("IMPR \n")
 
     def CRCTI(self, index):
-        self.code.append("CRTC " + self.tokens[index+2].value + '\n')
-        self.code.append("ARMZ " + self.tokens[index].value + '\n')
+        self.code.append("CRTC " + self.tokens[index].value + '\n')
 
     def CRVL(self, index):
         self.code.append("CRVL " + self.tokens[index].value + '\n')
@@ -192,18 +280,23 @@ class Translator():
         elif op == '>=':
             self.CMAG()
 
+    def counter(self, tokens, index):
+        while tokens[index].typ != 'ENDLINE':
+            index += 1
+        return index
+
     def NotPolonesa(self, tokens, index):
         pilha = []
-        print("entrou")
-        print(tokens[index])
-        while tokens[index].typ != 'ENDLINE':
+        pilha.append('(')
+        while (tokens[index].typ != 'ENDLINE'):
+
             if tokens[index].value == '(':
                 pilha.append(tokens[index].value)
                 index += 1
             elif tokens[index].value == ')':
-                while pilha[len(pilha-1)] != '(':
-                    code.append(pilha.pop())
-                print(pilha.pop())
+                while pilha[len(pilha) - 1] != '(':
+                    self.code.append(pilha.pop())
+                pilha.pop()
                 index += 1
             elif tokens[index].typ == 'ID':
                 self.CRVL(index)
@@ -231,6 +324,11 @@ class Translator():
                     self.CRCTI(index+1)
                 self.code.append("DIVI \n")
                 index += 2
+            elif tokens[index].typ == 'VIR':
+                break
+        while len(pilha) != 1:
+            self.code.append(pilha.pop())
+
 ##############################    MEPA     #################################################
 class Mepa():
     def __init__(self):
@@ -256,7 +354,7 @@ if __name__ == "__main__":
     mepa = Mepa()
     toke = lexico.tokenize(code)
 
-    texto = tradutor.translate(toke, 0)
+    texto = tradutor.translate(toke, 0, len(toke))
     print(texto)
 
     arq = open("traducao.txt", "w")
--- pascal.c
+++ pascal.c
@@ -1,19 +1,25 @@
+
 PROGRAM TESTE;
 	VAR N, K : INTEGER;
 	    F1, F2, F3 : INTEGER;
 BEGIN
 	READ(N);
-	F1:=0;
-	F2:=1;
-	K:=1;
+
 
 	WHILE K <= N DO
 	BEGIN
-		F3 := F1 + F2;
+		F3 :=  F1 + F2 ;
 		F1 := F2;
 		F2 := F3;
-		K := K + 1;
+		K := (K + 1);
 	END;
 
-	WRITE(N, F1);
+	WRITE(N, F1 + F2, F3);
+
+	IF A < 2 THEN
+		A := A + 1;
+	ELSE
+		B := B -1;
+	END;
+
 END;
